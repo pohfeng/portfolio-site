a13b9f321d0f5238ad7c956a20132ec6aa4dfbbc
pohfeng <pohfeng926@gmail.com>
2022-06-24 22:21:40 +0800

    test

--- components/About/About.js
+++ components/About/About.js
@@ -4,12 +4,7 @@ const About = () => {
       <h2 className="pt-20 text-4xl font-special-elite">About</h2>
       <div className="flex">
         <div className="text-base text-justify min-h-fit md:text-xl">
-          <p className="mb-3">
-            Hi, I am a self-taught Full Stack Developer with about four years of
-            experience developing web applications. After working hours, I spend
-            most of my time polishing my web development skills and making
-            myself a better developer every day.
-          </p>
+          <p className="mb-3">Hello Testing here.</p>
           {/* <p>
           I enjoyed working with
             skilled colleagues because cooperating with them made web app
